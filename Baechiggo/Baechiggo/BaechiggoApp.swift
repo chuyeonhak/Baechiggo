f344cdc7a675d48a6055df321c1d7adcac630b48
추연학 <=>
2024-01-04 16:25:00 +0900

    Feat: generate App

---
+++ Baechiggo/Baechiggo/BaechiggoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaechiggoApp.swift
+//  Baechiggo
+//
+//  Created by chuchu on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BaechiggoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
