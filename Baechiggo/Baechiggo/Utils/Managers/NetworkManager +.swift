0f1c65a3eeece4bb7b5b710c5db1ad1f375e2eb4
추연학 <=>
2024-01-08 13:20:59 +0900

    Feat: Add NetworkManager

--- Baechiggo/Baechiggo/Utils/Managers/NetworkManager +.swift
+++ Baechiggo/Baechiggo/Utils/Managers/NetworkManager +.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import Alamofire
+
+extension NetworkManager {
+    enum NetworkType {
+        case matchDataList
+        case getAllUser
+        
+        var header: HTTPHeaders {
+            return [
+                HTTPHeader(name: "Authorization", value: KeyManager.shared.getKey(.notionSecretKey)),
+                HTTPHeader(name: "Notion-Version", value: "2022-06-28")
+            ]
+        }
+        
+        var urlString: String {
+            let domain = "https://api.notion.com/v1/"
+            switch self {
+            case .matchDataList:
+                return domain + "blocks/\(KeyManager.shared.getKey(.matchListBlockID))/children"
+            case .getAllUser:
+                return domain + "users"
+            }
+        }
+        
+        var method: HTTPMethod {
+            switch self {
+            case .matchDataList:
+                return .get
+            case .getAllUser:
+                return .get
+            }
+        }
+        
+        var parameters: Parameters? {
+            switch self {
+            case .matchDataList, .getAllUser: nil
+            }
+        }
+        
+        var parameterEncoding: ParameterEncoding {
+            return JSONEncoding.default
+        }
+    }
+}
--- Baechiggo/Baechiggo/Utils/Managers/NetworkManager.swift
+++ Baechiggo/Baechiggo/Utils/Managers/NetworkManager.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import Alamofire
+
+struct NetworkManager {
+    static let shared = NetworkManager()
+    
+    private let notionHeaders : HTTPHeaders = [
+        HTTPHeader(name: "Authorization", value: KeyManager.shared.getKey(.notionSecretKey)),
+        HTTPHeader(name: "Notion-Version", value: "2022-06-28")
+    ]
+    
+    func requestAPI(_ type: NetworkType, completion: @escaping (AFDataResponse<Data?>) -> ()) {
+        AF.request(type.urlString,
+                   method: type.method,
+                   parameters: type.parameters,
+                   encoding: type.parameterEncoding,
+                   headers: type.header)
+        .validate(statusCode: 200..<600)
+        .response(completionHandler: completion)
+    }
+}
