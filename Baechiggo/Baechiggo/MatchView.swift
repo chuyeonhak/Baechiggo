6eda37f364c38068b944d1cdd0b5cdea2295f0c6
추연학 <=>
2024-01-08 13:21:14 +0900

    Feat: Add MatchView

--- Baechiggo/Baechiggo/MatchView.swift
+++ Baechiggo/Baechiggo/MatchView.swift
@@ -5,4 +5,68 @@
 //  Created by chuchu on 1/5/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct MatchView: View {
+    @State var isSpinnerShow: Bool = true
+    @State var matchList: [MatchDate] = []
+    
+    var body: some View {
+        ZStack {
+            
+            List(matchList, id: \.self) { matchData in
+                Text((matchData.childPage?.title)!)
+            }
+            .onAppear {
+                NetworkManager.shared.requestAPI(.matchDataList) {
+                    switch $0.result {
+                    case .success(let data):
+                        guard let data else { return }
+                        do {
+                            let json = try JSONDecoder().decode(MatchDateModel.self, from: data)
+                            matchList = json.matchDateList
+                            isSpinnerShow = false
+                        }
+                        catch {
+                            print(error)
+                        }
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+            }
+            
+            
+//            Button {
+//                isSpinnerShow.toggle()
+//            } label: {
+//                Text("Test")
+//            }.onAppear {
+//                NetworkManager.shared.requestAPI(.matchDataList) {
+//                    switch $0.result {
+//                    case .success(let data):
+//                        guard let data else { return }
+//                        do {
+//                            let json = try JSONDecoder().decode(MatchDateModel.self, from: data)
+//                            print(json)
+//                        }
+//                        catch {
+//                            print(error)
+//                        }
+//                    case .failure(let error):
+//                        print(error)
+//                    }
+//                }
+//                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//                    matchList = []
+//                    isSpinnerShow.toggle()
+//                }
+//            }
+
+            if isSpinnerShow {
+                SpinnerView()
+            }
+        }
+        
+    }
+}
--- Baechiggo/Baechiggo/RootView.swift
+++ Baechiggo/Baechiggo/RootView.swift
@@ -6,64 +6,18 @@
 //
 
 import SwiftUI
-import Alamofire
-
-//    let test = "https://www.notion.so/8ad11213234d4bf0a2bc41d10254d30c?v=74171ed6b12c4aba9d05a0f2967deadb&pvs=4"
-    
 
 struct RootView: View {
-    let api = TestAPI()
-    
     var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    NavigationLink("출석하기") { Text("출석하기") }
-                    NavigationLink("점수계산") { Text("점수 계산") }
+        ZStack {
+            NavigationView {
+                Form {
+                    Section {
+                        NavigationLink("출석하기") { Text("출석하기") }
+                        NavigationLink("점수계산") { MatchView() }
+                    }
                 }
             }
         }
     }
-}
-
-
-
-#Preview {
-    RootView()
-}
-
-struct TestAPI {
-    let notionHeaders : HTTPHeaders = [HTTPHeader(name: "Authorization", value: KeyManager.shared.getKey(.notionSecretKey)),
-                                       HTTPHeader(name: "Notion-Version", value: "2022-06-28")]
-    
-    func notionRead() {
-        
-//    https:www.notion.so/5b4f15d72e7243f38a8ab92d430ff54b?v=1c1af0d8a3d34ea5a214703edb000946&pvs=4
-//    https://www.notion.so/2250222c51e24d9d82a948c9a6715a4d?pvs=4
-//    https:www.notion.so/a41328cac58f48468a4471c33e0f53b4?v=bc98748f9bee4c0bbd66b5060bd80df0&pvs=4
-//    https://www.notion.so/2024-1-4-7edae5330cff4ba0bfc3012ffc139764?pvs=4
-        AF.request("https://api.notion.com/v1/blocks/\(KeyManager.shared.getKey(.matchListBlockID))/children",
-                   method: .get,
-                   parameters: nil,
-                   encoding: JSONEncoding.default,
-                   headers: notionHeaders,
-                   interceptor: nil,
-                   requestModifier: nil).validate(statusCode: 200..<600).response { response in
-            
-            switch response.result {
-            case .success(let data):
-                guard let data = data else { return }
-                
-                do {
-                    guard let prettyJson = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return }
-                    print("get Json : \(prettyJson)")
-                } catch {
-                    print("파싱 에러 : \(error)")
-                }
-            case .failure(let error):
-                print("응답 에러 : \(error)")
-            }
-        }
-    }
-    init() { notionRead() }
 }
