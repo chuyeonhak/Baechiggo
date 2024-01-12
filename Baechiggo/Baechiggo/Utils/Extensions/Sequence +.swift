51699338fe6e5bf343060ee53bfa1f03993c6575
추연학 <=>
2024-01-12 10:04:58 +0900

    Feat: Add Sequence Extension

--- Baechiggo/Baechiggo/Utils/Extensions/Sequence +.swift
+++ Baechiggo/Baechiggo/Utils/Extensions/Sequence +.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+extension Sequence {
+    func asyncMap<T>(
+        _ transform: (Element) async throws -> T
+    ) async rethrows -> [T] {
+        var values = [T]()
+        
+        for element in self {
+            try await values.append(transform(element))
+        }
+        
+        return values
+    }
+    
+    func asyncCompactMap<T>(
+        _ transform: (Element) async throws -> T?
+    ) async rethrows -> [T] {
+        var values = [T]()
+        
+        for element in self {
+            guard let value = try await transform(element) else { continue }
+            values.append(value)
+        }
+        
+        return values
+    }
+}
